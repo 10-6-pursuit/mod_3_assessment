6acda60af9cc04bae36e77f4bd5d7bb1d11f2f64
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-08-10 13:01:31 -0400

    frontend done without styling

--- frontend/src/Components/Animes.js
+++ frontend/src/Components/Animes.js
@@ -1,11 +1,29 @@
 import Anime from "./Anime";
+import { useEffect, useState } from "react";
+const URL = process.env.REACT_APP_API_URL;
 
 function Animes() {
   //fetch all animes from the backend and render them as a list using the Anime component. Make sure to style the animes to look like the screenshot from the README. Feel free to use axios to grab data
+  const [animes, setAnimes] = useState([]);
+
+  useEffect(() => {
+    fetch(`${URL}/animes`)
+      .then((res) => res.json())
+      .then((data) => setAnimes(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  console.log(animes);
 
   return (
     <section className="index" id="anime-list">
-      <p>No animes to display</p>
+      {animes.map((anime) => (
+        <Anime
+          key={anime.id}
+          name={anime.name}
+          description={anime.description}
+        />
+      ))}
     </section>
   );
 }
